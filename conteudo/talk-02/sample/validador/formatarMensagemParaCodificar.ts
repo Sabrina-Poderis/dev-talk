31c139be9ed7f531dced9a2755bc0e02d5a7236e
Sabrina Poderis <act172@einstein.br>
2024-09-19 17:55:37 -0300

    feature: create validator formatarMensagemParaCodificar

---
+++ conteudo/talk-02/sample/validador/formatarMensagemParaCodificar.ts
@@ -0,0 +1,9 @@
+const alfabetoPadrao = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÇÃÉÀÊÓÁÔÍ0123456789abcdefghijklmnopqrstuvwxyzçãéàêóáôí'
+
+const formatarMensagemParaCodificar = (mensagem: string) : string => {
+  mensagem.split('').map((caracter: string) => {
+    
+  })
+}
+
+export default formatarMensagemParaCodificar
