2fdbcd1da5923ea2b4e20b6212dc6abe6995b887
Sabrina Poderis <act172@einstein.br>
2024-09-24 15:41:39 -0300

    feature: implement validator formatarMensagemParaCodificar

--- conteudo/talk-02/sample/index.ts
+++ conteudo/talk-02/sample/index.ts
@@ -1,6 +1,9 @@
+import formatarMensagemParaCodificar from "./validador/formatarMensagemParaCodificar.ts";
+
 function main () {
   const entradaDigitadaNoTerminal = process.argv[2];
-  console.log(entradaDigitadaNoTerminal)
+  const mensagemFormatada= formatarMensagemParaCodificar(entradaDigitadaNoTerminal)
+  console.log(mensagemFormatada)
 }
 
 main()
--- conteudo/talk-02/sample/validador/formatarMensagemParaCodificar.ts
+++ conteudo/talk-02/sample/validador/formatarMensagemParaCodificar.ts
@@ -1,9 +1,16 @@
 const alfabetoPadrao = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÇÃÉÀÊÓÁÔÍ0123456789abcdefghijklmnopqrstuvwxyzçãéàêóáôí'
 
 const formatarMensagemParaCodificar = (mensagem: string) : string => {
-  mensagem.split('').map((caracter: string) => {
-    
+  const mensagemFormatada = mensagem.split('').map((caracter: string) => {
+    if(alfabetoPadrao.includes(caracter)){
+      return caracter
+    } else {
+      return '?'
+    }
   })
+
+  console.log(mensagemFormatada)
+  return mensagemFormatada.join('')
 }
 
 export default formatarMensagemParaCodificar
