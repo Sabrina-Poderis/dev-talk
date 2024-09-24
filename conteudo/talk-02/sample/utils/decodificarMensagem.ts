705265d95e700f00f44512880dbb740bb0be3aa8
Sabrina Poderis <act172@einstein.br>
2024-09-24 16:11:46 -0300

    feature: create decodificarMensagem

---
+++ conteudo/talk-02/sample/utils/decodificarMensagem.ts
@@ -0,0 +1,19 @@
+import alfabetoPadrao from "../constantes/alfabetoPadrao.ts";
+
+const decodificarMensagem = (mensagem: string, intervalo: number) : string => {
+  const mensagemCodificada = mensagem.split('').map((caracter: string) => {
+    const posicaoAtual = alfabetoPadrao.indexOf(caracter);
+    const acabouAlfabeto = posicaoAtual + 1 >= alfabetoPadrao.length
+
+    if(acabouAlfabeto){
+      const novaPosicao = (alfabetoPadrao.length - 1) - posicaoAtual
+      return alfabetoPadrao[novaPosicao - intervalo]
+    } else {
+      return alfabetoPadrao[posicaoAtual - intervalo]
+    }
+  })
+
+  return mensagemCodificada.join('')
+}
+
+export default decodificarMensagem
